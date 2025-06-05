3ff4b1aeb893a3bbb4121600f663c688fc535b4a
melonmann12 <hongquanvjp@gmail.com>
2025-06-05 14:42:42 +0700

    StaffDashboard
    hih

--- src/main/java/com/example/swp/entity/Contact.java
+++ src/main/java/com/example/swp/entity/Contact.java
@@ -13,6 +13,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+
 public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,14 +24,17 @@ public class Contact {
     private Date dateFrom;
     private Date dateTo;
     private double amount;
+
     //customer 1-many
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
+
     //staff 1-many
     @ManyToOne
     @JoinColumn(name = "staff_id")
     private Staff staff;
+
     //storage 1-many
     @ManyToOne
     @JoinColumn(name = "storage_id")
--- src/main/java/com/example/swp/service/impl/EmailServiceImpl.java
+++ src/main/java/com/example/swp/service/impl/EmailServiceImpl.java
@@ -12,16 +12,16 @@ import org.springframework.stereotype.Service;
 public class EmailServiceImpl implements EmailService {
         @Autowired
         private JavaMailSender sendEmail;
-
         public void sendEmail(String to, String subject, String body) {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom("shop@gmail.com"); // Email gửi đi
             message.setTo(to);
             message.setSubject(subject);
             message.setText(body);
-
             sendEmail.send(message);
+
         }
+
     }
 
 
