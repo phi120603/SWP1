7240173c847802a374880ef8a1f869927f528e1f
phi120603 <luuphilong12062003@gmail.com>
2025-06-11 10:49:53 +0700

    fix front-end

--- src/main/java/com/example/swp/controller/website/HomeController.java
+++ src/main/java/com/example/swp/controller/website/HomeController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/SWP")
+@RequestMapping("/SWP")
 public class HomeController {
-    @GetMapping("/index")
+    @GetMapping("/home-page")
     public String returnhome (Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
         model.addAttribute("username", username);
-        return"index";
+        return"home-page";
     }
     @GetMapping("/")
     public String returnmain () {
-        return"redirect:index";
+        return"redirect:home-page";
     }
 
 }
