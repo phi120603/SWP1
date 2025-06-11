36fab9a226ffedda996a9bc018ca37be21546857
phi120603 <luuphilong12062003@gmail.com>
2025-06-11 11:34:26 +0700

    sua login

--- src/main/java/com/example/swp/controller/website/HomeController.java
+++ src/main/java/com/example/swp/controller/website/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/SWP")
+//@RequestMapping("/SWP")
 public class HomeController {
     @GetMapping("/home-page")
     public String returnhome (Model model) {
