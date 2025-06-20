1c4c2c4c6ef638ce98dd0622d48fd103699379d5
Quan282 <quannmhe186996@fpt.edu.vn>
2025-06-20 09:01:06 +0700

    uo

--- src/main/java/com/example/swp/service/impl/FeedbackServiceImpl.java
+++ src/main/java/com/example/swp/service/impl/FeedbackServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.swp.service.impl;
 
+import com.example.swp.entity.Customer;
 import com.example.swp.entity.Feedback;
+import com.example.swp.entity.Storage;
+import com.example.swp.repository.CustomerRepository;
 import com.example.swp.repository.FeedbackRepository;
+import com.example.swp.repository.OrderRepository;
+import com.example.swp.repository.StorageReponsitory;
 import com.example.swp.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +19,27 @@ public class FeedbackServiceImpl implements FeedbackService {
 
     @Autowired
     private FeedbackRepository feedbackRepository;
+
+    @Autowired
+    private OrderRepository orderRepo;
+
+    @Autowired
+    private CustomerRepository customerRepo;
+
+    @Autowired
+    private StorageReponsitory storageRepo;
+
+
+    @Override
+    public void deleteFeedback(int id) {
+        feedbackRepository.deleteById(id);
+    }
+    @Override
+    public boolean existsByCustomerAndStorage(Customer customer, Storage storage) {
+        return feedbackRepository.existsByCustomerAndStorage(customer, storage);
+    }
+
+
 
     @Override
     public List<Feedback> getAllFeedbacks() {
@@ -38,5 +64,20 @@ public class FeedbackServiceImpl implements FeedbackService {
     @Override
     public Feedback save(Feedback feedback) {
         return feedbackRepository.save(feedback);
+    }
+
+    @Override
+    public Feedback createOrUpdateFeedback(int storageId, int customerId, String content, int rating) {
+        return null;
+    }
+
+    @Override
+    public Feedback createFeedback(int storageId, int customerId, String content, int rating) {
+        return null;
+    }
+
+    @Override
+    public Optional<Feedback> findByCustomerAndStorage(Customer customer, Storage storage) {
+        return Optional.empty();
     }
 }
